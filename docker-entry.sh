7af6e6609886e8fd88a49cb88ebc5edf9084d388
Rasmus Munk <munk1@live.dk>
2023-01-02 14:28:05 +0100

    Update the image such that it removes the SSH keys generated by the openssh-server installation and add the docker-entry.sh script to reconfigure new keys when the image is launched

---
+++ docker-entry.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Generate a new set of host SSH keys for the OpenSSH server
+echo "Reconfiguring OpenSSH Server"
+/usr/sbin/dpkg-reconfigure openssh-server
+
+echo "Running the OpenSSH Server"
+# Launch the main.py script that starts the OpenSSH server daemon
+/usr/bin/python3 /app/main.py
